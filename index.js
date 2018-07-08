4974ff1b9a18ba9fdfcd7f30c35b0210e123262c
Mitch Hankins <mitchhankins92@gmail.com>
2018-07-07 18:47:41 -0600

    Adding forecast, help, and version commands

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ const minimist = require('minimist');
 
 module.exports = () => {
   const args = minimist(process.argv.slice(2));
-  const command = args._[0];
+  let command = args._[0] || 'help';
+
+  if (args.help || args.h) {
+    command = 'help';
+  }
+  if (args.version || args.v) {
+    command = 'version';
+  }
 
   switch (command) {
     case 'today':
       return require('./commands/today')(args);
+    case 'help':
+      return require('./commands/help')(args);
+    case 'version':
+      return require('./commands/version')(args);
+    case 'forecast':
+      return require('./commands/forecast')(args);
     default:
       return console.log(`"${command}" is not a valid command.`);
   }
